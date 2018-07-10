94d0f15c8e8299e2ca4f312ad10bc94350209232
Toby Johnston <toby@crssoftware.co.nz>
2018-07-10 22:20:59 +1200

    boo filenames

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { browserHistory } from 'react-router';
 import HomePage from './HomePage';
 import NavBar from './HeaderComponent/NavBar';
+import Footer from './HeaderComponent/Footer';
 class App extends Component {
   render() {
     return (
